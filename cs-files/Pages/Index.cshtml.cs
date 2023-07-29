5e4c18ba88ae35dfb4ec31b57162f7944a6cc952
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2023-07-28 22:33:22 -0300

    Implementada interface do site

--- cs-files/Pages/Index.cshtml.cs
+++ cs-files/Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.Models;
+using ContosoCrafts.Services;
 
-namespace cs_files.Pages;
+namespace ContosoCrafts.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public JsonFileProductService ProductService;
+    public IEnumerable<Product> Produtos {get; private set;}
+    public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
     {
         _logger = logger;
+        ProductService = productService;
     }
 
     public void OnGet()
     {
-
+        Produtos = ProductService.GetProducts();
     }
 }
