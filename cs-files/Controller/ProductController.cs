876f67f7116703a639690abfd92f5f3747f57f6f
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2023-08-06 22:54:00 -0300

    Mapeando endpoint para JSON

---
+++ cs-files/Controller/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ContosoCrafts.Services;
+using ContosoCrafts.Models;
+
+namespace ContosoCrafts.Controller;
+
+[Route("product")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    public ProductController(JsonFileProductService productService)
+    {
+        this.ProductService = productService;
+    }
+
+    public JsonFileProductService ProductService { get; }
+
+    [HttpGet]
+    public IEnumerable<Product>? Get()
+    {
+        return ProductService.GetProducts();
+    }
+}
--- cs-files/Program.cs
+++ cs-files/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddTransient<JsonFileProductService>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -24,5 +25,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapControllers();
 
 app.Run();
