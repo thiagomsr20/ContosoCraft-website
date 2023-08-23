d7690d85be29ebf027eb1a898c25c92b67475f76
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2023-08-23 13:15:16 -0300

    Trabalhando no metodo removemModel

--- teste/Program.cs
+++ teste/Program.cs
@@ -3,17 +3,12 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using teste.model;
-using System.IO;
 
 namespace ProgramSpace;
 
 public class TesteModeloServices
 {
-    // public string DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
     public string JsonFile => Path.Combine("Data/Teste.json");
-
-    // Reescrevendo método ToString, para facilitar a visualização do database
-    public override string ToString() => JsonSerializer.Serialize<List<TesteModelo>>(GetModelos());
 
     public List<TesteModelo> GetModelos()
     {
@@ -44,35 +39,40 @@ public class TesteModeloServices
     }
 
     // Método RemoveTestModel ainda incompleto
-    public void RemoveTestModel(string id)
+    public void RemoveTestModel(string id, List<TesteModelo> testeModelosList)
     {
-        if(string.IsNullOrEmpty(id)) throw new Exception("Null or empty string input");
+        var product = testeModelosList.First(product => product.Id == id);
+        if (product is null) throw new ArgumentNullException();
 
-        List<TesteModelo> TestModelList = GetModelos();
-        TestModelList.Remove(TestModelList.First(product => product.Id == id));
+        testeModelosList.Remove(product);
+
+        using (var outputStream = File.OpenWrite(JsonFile))
+        {
+            JsonSerializer.Serialize<List<TesteModelo>>(
+                new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                {
+                    Indented = true,
+                    SkipValidation = true
+                }),
+                testeModelosList
+            );
+        }
+
     }
 }
 
-
-class ProgramSpace
+class ProgramSpaced
 {
     static void Main(string[] args)
     {
         TesteModeloServices service = new TesteModeloServices();
         List<TesteModelo> lista = service.GetModelos();
-        // service.RemoveTestModel("Marlon");
-        // service.RemoveTestModel("Thiago");
-        // service.RemoveTestModel("");
-        
-        // Tentando verificar o que está sendo retornado e como está funcionando o teste com JSON
-        Console.WriteLine(lista.ToString());
+        service.RemoveTestModel("Thiago", lista);
 
-        // foreach (var testeModelo in service.GetModelos())
-        // {
-        //     Console.WriteLine(testeModelo.Id);
-        //     Console.WriteLine(testeModelo.Value);
-        // }
-        // Console.WriteLine($"That is your path: {service.JsonFile}");
-
+        foreach (var itens in lista)
+        {
+            Console.WriteLine(itens.Id);
+            Console.WriteLine(itens.Value);
+        }
     }
 }
